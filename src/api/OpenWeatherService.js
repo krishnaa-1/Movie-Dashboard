e4a126dc10f11d3df75a93b44f21c3b3d5346b5e
krishnaa-1 <krishnamalviya272@gmail.com>
2024-07-05 10:50:19 +0530

    done

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,8 +1,6 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
-
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
-
+const WEATHER_API_KEY = 'be82502faf13c0ea744fcfdef2b678e4';
 const GEO_API_OPTIONS = {
   method: 'GET',
   headers: {
